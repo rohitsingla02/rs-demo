e4a541ee4bc49ba0928ff9dd821e08d1ab1b24bb
rohitsingla02 <59019446+rohitsingla02@users.noreply.github.com>
2020-02-28 20:33:36 +0530

    Update aws-infra-provisioning-template.tf

--- aws-infra-provisioning-template.tf
+++ aws-infra-provisioning-template.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_instance" "instance" {
 
-  ami = "ami-018d8a3a5c21dfd01"
+  ami = "ami-087ca3b8702bd90cc"
   instance_type = "t2.micro"
   key_name = "packer-infy-key"
   security_groups = ["packer-infysec-1"]
