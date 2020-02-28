d928ed54d553269c3f690df94a408619552796fe
rohitsingla02 <59019446+rohitsingla02@users.noreply.github.com>
2020-02-28 18:20:45 +0530

    Update main-sg-poc.tf

--- main-sg-poc.tf
+++ main-sg-poc.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "instance" {
 
   ami = "ami-0a58581a892ec8554"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name = "packer-infy-key"
   security_groups = ["packer-infysec-1"]
   tags = {
